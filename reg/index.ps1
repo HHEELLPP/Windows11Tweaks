22fddb435082de6108ffcbb457a68cde6b25184a
HHEELLPP <69455204+HHEELLPP@users.noreply.github.com>
2024-10-15 21:18:09 -0600

    .

--- reg/index.ps1
+++ reg/index.ps1
@@ -8,18 +8,29 @@ public class User32 {
 }
 "@
 Invoke-WebRequest  `
--Uri (ConvertFrom-Json (Invoke-WebRequest -Uri "https://api.github.com/repos/HHEELLPP/Windows11Tweaks/contents/index.reg").Content).download_url `
--OutFile ".\index.reg"
+-Uri (ConvertFrom-Json (Invoke-WebRequest -Uri "https://api.github.com/repos/HHEELLPP/Windows11Tweaks/contents/index.json").Content).download_url `
+-OutFile ".\index.json"
 
 taskkill /f /im explorer.exe 2>&1> $null
-reg import ".\index.reg"
+
+$aadadf = Get-Content ".\index.json" | ConvertFrom-Json -Depth 5
+foreach ($aa in $aadadf.PSObject.Properties) {
+	$Path = $aa.Name
+	foreach ($bb in $aa.Value.PSObject.Properties) {
+		$Name = $bb.Name
+		$Value = $bb.Value.Value
+		$Type = $bb.Value.Type
+		Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type $Type
+	}
+}
+
 $SPI_SETDESKWALLPAPER = 0x0014
 $SPIF_UPDATEINIFILE = 0x01
 $SPIF_SENDCHANGE = 0x02
-[User32]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, [IntPtr]::Zero, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+[User32]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, "$env:SystemRoot\web\wallpaper\Windows\img19.jpg", $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
 Start-Process explorer.exe
 
-Remove-Item -LiteralPath ".\index.reg"
+Remove-Item -LiteralPath ".\index.json"
 
 $Creds = Get-Credential
 Restart-Computer -Credential $Creds -Force
