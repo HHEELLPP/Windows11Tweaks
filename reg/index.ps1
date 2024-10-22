b1116d3e055a3b5465ebc1cea9e003f77e79c17f
HHEELLPP <69455204+HHEELLPP@users.noreply.github.com>
2024-10-21 18:11:53 -0600

    .

--- reg/index.ps1
+++ reg/index.ps1
@@ -17,30 +17,24 @@ foreach ($aa in $aadadf.PSObject.Properties) {
 		$Name = $bb.Name
 		$Value = $bb.Value.Value
 		$Type = $bb.Value.Type
+		[PSCustomObject]@{
+			Name = $Name
+			Value = $Value
+			Type = $Type
+		}
 		# Write-Output $Type
 		if($Type -ieq "BINARY") {
-			Write-Output $Value
-			if($Value.ToLower().StartsWith("hex:")) {
-				[byte[]]$Value = $Value.Substring(4)
-				Write-Output $Value
-				exit
-			}
+			$Value = $Value -Split ","
 		}
-		# Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type $Type
+		Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type $Type
 	}
 }
 
-$SPI_SETDESKWALLPAPER = 0x0014
-$SPIF_UPDATEINIFILE = 0x01
-$SPIF_SENDCHANGE = 0x02
-[User32]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, [IntPtr]::Zero, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+# $SPI_SETDESKWALLPAPER = 0x0014
+# $SPIF_UPDATEINIFILE = 0x01
+# $SPIF_SENDCHANGE = 0x02
+# [User32]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, [IntPtr]::Zero, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
 Start-Process explorer.exe
 
 # $Creds = Get-Credential
 # Restart-Computer -Credential $Creds -Force
-# HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent: [
-# 		"AccentPalette": {
-# 			"Type": "BINARY",
-# 			"Value": "ec,c6,f7,00,d2,a5,ed,00,a6,6a,dc,00,95,54,d5,00,74,31,c9,00,4e,25,99,00,21,0f,6a,00,88,17,98,00"
-# 		},
-#   ]
