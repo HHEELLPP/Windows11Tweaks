ce3fc1f84443790511f3d696303b66a7356fc414
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-11-06 13:25:18 -0700

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -90,5 +90,3 @@ New-ItemProperty -LiteralPath 'HKCU:\Software\Microsoft\Windows\DWM' -Name 'UseD
 Start-Process explorer.exe
 
 'Windows ' + [System.Environment]::OSVersion.Version.Major
-
-$null = $host.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”)
