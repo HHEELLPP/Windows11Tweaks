7a6b47b2d988b00082fa57e7b3c6b9a0cc9a0ab0
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-11-11 10:21:35 -0700

    Update getAndRun.ps1

--- getAndRun.ps1
+++ getAndRun.ps1
@@ -1,2 +1,3 @@
+winget install --id Microsoft.PowerShell --scope user --exact --accept-source-agreements --accept-package-agreements
 PowerShell.exe -WindowStyle "Minimized" "Invoke-Expression (Invoke-WebRequest (Invoke-WebRequest 'https://api.github.com/repos/HHEELLPP/Windows11Tweaks/contents/plsWork.ps1' | ConvertFrom-Json).download_url)"
 PowerShell.exe -WindowStyle "Minimized" "Invoke-Expression (Invoke-WebRequest (Invoke-WebRequest 'https://api.github.com/repos/HHEELLPP/Windows11Tweaks/contents/index.ps1' | ConvertFrom-Json).download_url)"
