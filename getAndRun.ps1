e0f5b39faeb0e2f41fd449962a680d9dd15decdf
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-11-11 13:28:01 -0700

    Update getAndRun.ps1

--- getAndRun.ps1
+++ getAndRun.ps1
@@ -1,3 +1,3 @@
 winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
-pwsh.exe -WindowStyle "Minimized" "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.ps1')"
-pwsh.exe -WindowStyle "Minimized" "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/index.ps1')"
+pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.ps1')"
+pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/index.ps1')"
