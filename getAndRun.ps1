1420dde0ee966a7f92304d212969f22000975e03
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-11-11 13:57:02 -0700

    Update getAndRun.ps1

--- getAndRun.ps1
+++ getAndRun.ps1
@@ -2,9 +2,12 @@
 # pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.ps1')"
 # pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/index.ps1')"
 if($PSVersionTable.PSEdition -neq 'Core') {
-	try {
-		pwsh.exe -File $PSCommandPath
-	} catch {
+	if(-not (Test-CommandExists pwsh.exe)) {
 		winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
 	}
+	pwsh.exe -File $PSCommandPath
 }
+'Tada'
+$Host.UI.Write('Press any key to continue . . . ')
+$null = $Host.UI.RawUI.ReadKey(6)
+$Host.UI.WriteLine()
