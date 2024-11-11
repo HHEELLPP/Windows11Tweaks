1a15ec16c61671e807ba68e2c907ffc96e265420
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-11-11 13:47:24 -0700

    Update getAndRun.ps1

--- getAndRun.ps1
+++ getAndRun.ps1
@@ -1,3 +1,26 @@
-winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
-pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.ps1')"
-pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/index.ps1')"
+# winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
+# pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.ps1')"
+# pwsh.exe -w "Minimized" -c "Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/index.ps1')"
+if($PSVersionTable.PSEdition -neq 'Core') {
+  try {
+    pwsh.exe -File $PSCommandPath
+  } catch {
+    winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
+  }
+}
+
+function Test-CommandExists {
+	param($command)
+	$oldPreference = $ErrorActionPreference
+	$ErrorActionPreference = ‘stop’
+	try {
+		if(Get-Command $command) {
+			return $true
+		}
+	} catch {
+		Write-Host “$command does not exist”
+		return $false
+	} finally {
+		$ErrorActionPreference = $oldPreference
+	}
+}
