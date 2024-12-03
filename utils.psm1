273ecd1930632741ae0ab00966256ad76650e2f9
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-12-03 12:25:35 -0700

    Update utils.psm1

--- utils.psm1
+++ utils.psm1
@@ -19,7 +19,7 @@ function Stop-Program {
 	param()
 	$Host.UI.Write('Press any key to continue . . . ')#'Press Enter to continue...: '
 	<# https://learn.microsoft.com/dotnet/api/system.management.automation.host.readkeyoptions#fields #>
-	$null = $Host.UI.RawUI.ReadKey(0b0110)#6 = 0b0110
+	$null = $Host.UI.RawUI.ReadKey(6)#6 = 0b0110
 	$Host.UI.WriteLine()
 }
 function Get-PowerShellEdition {
