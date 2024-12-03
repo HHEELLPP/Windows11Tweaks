94b7bbb3751a16780a6cdba81a6210deb85543e6
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-12-03 12:23:13 -0700

    Update pwsh.ps1

--- pwsh.ps1
+++ pwsh.ps1
@@ -1,4 +1,4 @@
 Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/utils.psm1')
-if(-not (Test-CommandExists pwsh.exe)) {
+if(Test-CommandExists pwsh.exe -Invert) {
 	winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
 }
