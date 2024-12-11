cc1aa51be4793f3ed997406537d0a1fd96533c71
Luna <69455204+HHEELLPP@users.noreply.github.com>
2024-12-11 13:40:42 -0700

    Update plsWork.ps1

--- plsWork.ps1
+++ plsWork.ps1
@@ -4,7 +4,7 @@ if(Test-CommandExists pwsh.exe -Invert) {
 	winget.exe install --id Microsoft.PowerShell --source winget --scope user --exact --accept-source-agreements --accept-package-agreements
 }
 
-if($PSVersionTable.PSEdition -neq 'Core') {
+if($PSVersionTable.PSEdition -ne 'Core') {
 	pwsh.exe -NoExit -c "Invoke-WebRequest 'https://raw.githubusercontent.com/HHEELLPP/Windows11Tweaks/refs/heads/master/plsWork.psm1' | Invoke-Expression"
  	exit 0
 }
